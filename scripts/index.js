97dab58ab91ee120562a11e87c07b86d76250dd1
Alessandro Amormino <amormino.alessandro97@gmail.com>
2022-06-30 15:37:47 +0200

    add animated logo

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,21 @@
 const loader = document.querySelector('#bm');
-const main = document.querySelector('.all-content');
+// const main = document.querySelector('.all-content');
 
 
-function init() {
-  setTimeout(() => {
-    loader.style.opacity = 0;
-    loader.style.display = 'none';
+// function init() {
+//   setTimeout(() => {
+//     loader.style.opacity = 0;
+//     loader.style.display = 'none';
 
-    main.style.display = 'block';
-    setTimeout(() => (main.style.opacity = 1), 50);
-  }, 2000);
-}
-
-// setTimeout(myURL, 2000);
-
-// function myURL(){
-//    window.open('index.html');
+//     main.style.display = 'block';
+//     setTimeout(() => (main.style.opacity = 1), 50);
+//   }, 2000);
 // }
-
 
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: './scripts/LOADING.json'
+    path: './scripts/LOGO.json'
 })
-
-
-init();
