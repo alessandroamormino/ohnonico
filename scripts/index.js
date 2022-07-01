5049600946f3a96a43d3809d3d4b35d071cdfdd5
Alessandro Amormino <amormino.alessandro97@gmail.com>
2022-07-01 16:08:11 +0200

    major fix and changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,36 @@
+const loaderBlack = document.querySelector('#bm-black');
+const loaderYellow = document.querySelector('#bm-yellow');
 const loader = document.querySelector('#bm');
-// const main = document.querySelector('.all-content');
 
 
-// function init() {
-//   setTimeout(() => {
-//     loader.style.opacity = 0;
-//     loader.style.display = 'none';
-
-//     main.style.display = 'block';
-//     setTimeout(() => (main.style.opacity = 1), 50);
-//   }, 2000);
-// }
+// var animation = bodymovin.loadAnimation({
+//     container: document.getElementById('bm'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: './scripts/LOGO.json'
+// })
 
 var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './scripts/LOGO.json'
+  container: document.getElementById('bm-black'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './scripts/LOGO_NERO.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './scripts/LOGO_NERO.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm-yellow'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './scripts/LOGO_GIALLO.json'
 })
